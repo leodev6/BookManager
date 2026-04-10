b610db326fe790bf64c28f05ffd440fd485900c3
leodev6 <djouakalionel4@gmail.com>
2026-04-10 22:08:14 +0200

    Implementation de la configuration de Port

---
+++ src/main/java/it/codeaveclionel/bookmanager/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package it.codeaveclionel.bookmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.http.CacheControl.maxAge;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localohost:4200")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+
+}
